3b2c5024476666e68554ec21bffbdc368397e15a
dhrn <dharan.g@muraai.com>
2021-07-16 03:01:03 +0530

    * fix typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 ## start env
 // todo remove this
-docker system prune -a --volumes -f
+docker system prune -a --volumes -f;
 
 docker-compose up -d;
 sleep 60;
@@ -10,4 +10,5 @@ docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=alice,
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=alice,ou=People,dc=example,dc=com bob"
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=httpalfresco,ou=People,dc=example,dc=com httpalfresco"
 
-
+#docker exec -ti kerberos mkdir -p /opt/
+docker exec -ti kerberos printf "%b" "add_entry -password -p httpalfresco@EXAMPLE.COM -k 1 -e aes256-cts-hmac-sha1-96\npassword\nwrite_kt alfresco.keytab" | ktutil
