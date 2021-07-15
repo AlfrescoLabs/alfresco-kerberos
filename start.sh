89c2c6f03bce46652be8ea506078d23443c1a9db
dhrn <dharan.g@muraai.com>
2021-07-16 03:33:03 +0530

    * fix typo

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=alice
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=httpalfresco,ou=People,dc=example,dc=com httpalfresco"
 
 #docker exec -ti kerberos mkdir -p /opt/
-docker exec -ti kerberos printf "%b" "add_entry -password -p httpalfresco@EXAMPLE.COM -k 1 -e aes256-cts-hmac-sha1-96\npassword\nwrite_kt alfresco.keytab" | ktutil
+#docker exec -ti kerberos printf "%b" "add_entry -password -p httpalfresco@EXAMPLE.COM -k 1 -e aes256-cts-hmac-sha1-96\npassword\nwrite_kt alfresco.keytab" | ktutil
