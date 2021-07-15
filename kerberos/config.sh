63ccc45ab45a83f63f89382b230d5ceecb2d3537
dhrn <dharan.g@muraai.com>
2021-07-15 21:24:02 +0530

    * fix typo

--- kerberos/config.sh
+++ kerberos/config.sh
@@ -77,13 +77,13 @@ create_db() {
 }
 
 init_ldap() {
-  kdb5_ldap_util -D $LDAP_USER,$LDAP_DC create -subtrees $LDAP_DC -r $REALM -s -H $LDAP_URL <<EOF
+  kdb5_ldap_util -D cn=$LDAP_USER,$LDAP_DC create -subtrees $LDAP_DC -r $REALM -s -H $LDAP_URL <<EOF
 $LDAP_PASS
 $KERB_ADMIN_PASS
 $KERB_ADMIN_PASS
 EOF
 
-  kdb5_ldap_util -D $LDAP_USER,$LDAP_DC stashsrvpw -f /etc/krb5kdc/service.keyfile $LDAP_USER,$LDAP_DC <<EOF
+  kdb5_ldap_util -D cn=$LDAP_USER,$LDAP_DC stashsrvpw -f /etc/krb5kdc/service.keyfile cn=$LDAP_USER,$LDAP_DC <<EOF
 $LDAP_PASS
 $LDAP_PASS
 $LDAP_PASS
@@ -101,7 +101,7 @@ restart_kdc() {
 }
 
 create_admin_user() {
-  kadmin.local -q "addprinc -x $KERB_ADMIN_USER,$LDAP_DC admin" <<EOF
+  kadmin.local -q "addprinc -x cn=$KERB_ADMIN_USER,$LDAP_DC admin" <<EOF
 $LDAP_PASS
 $LDAP_PASS
 EOF
@@ -111,10 +111,10 @@ EOF
 if [ ! -f /kerberos_initialized ]; then
   mkdir -p /var/log/kerberos
 
+  create_db
   init_ldap
   create_config
   create_admin_user
-  create_db
   start_kdc
 
 #  touch /kerberos_initialized
