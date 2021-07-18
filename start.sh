b3f8d8a523c9caef1a1169d7a70a5908774986f3
dhrn <dharan.g@muraai.com>
2021-07-18 17:09:11 +0530

    * fix

--- start.sh
+++ start.sh
@@ -1,29 +1,29 @@
-printf "\n======== stop the running env ========\n"
+printf "\n======== stop the running env ========\n\n"
 
 docker-compose down -v;
 sleep 10;
 rm -rf ./keytabs/*keytab;
 
-printf "\n======== build and run docker images ========\n"
+printf "\n======== build and run docker images ========\n\n"
 docker-compose build;
 docker-compose up -d;
 sleep 60;
 
-printf "\n======== indexing the LDAP user with Kerberos ========\n"
+printf "\n======== indexing the LDAP user with Kerberos ========\n\n"
 
 ## index ldap user
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=alice,ou=People,dc=example,dc=com alice"
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=bob,ou=People,dc=example,dc=com bob"
 
-printf "\n======== kerberos principles for server ========\n"
-# Add principles for KeyCloak and generate keytab
+printf "\n======== create kerberos principles for server ========\n\n"
+# Add principles for Alfresco and generate keytab
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpalfresco,ou=People,dc=example,dc=com HTTP/example.com@EXAMPLE.COM"
 docker exec -ti kerberos kadmin.local -q "ktadd -k alfresco.keytab HTTP/example.com@EXAMPLE.COM"
 
-printf "\n======== Available principles in the kerberos ========\n"
+printf "\n======== Available principles in the kerberos ========\n\n"
 docker exec -ti kerberos kadmin.local -q "list_principals"
 
-printf "\n======== configuring the Alfresco with Kerberos ========\n"
+printf "\n======== configure the Alfresco with Kerberos ========\n\n"
 KERBEROS=$(docker-compose ps -q kerberos);
 ALFRESCO=$(docker-compose ps -q alfresco);
 docker cp ${KERBEROS}:/alfresco.keytab ./keytabs/
@@ -32,5 +32,5 @@ docker cp ./keytabs/alfresco.keytab ${ALFRESCO}:/etc/alfresco.keytab
 
 docker-compose restart alfresco
 
-printf "\n======== kerberos configuration is over. Here is the tail ========\n"
+printf "\n======== kerberos configuration is over. Here is the tail ========\n\n"
 docker logs -f alfresco
