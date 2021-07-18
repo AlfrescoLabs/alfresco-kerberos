76aff18e003a3c0cac7beb444254513ece2c4eb8
dhrn <dharan.g@muraai.com>
2021-07-18 16:38:37 +0530

    * fix

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ sleep 60;
 
 ## index ldap user
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=alice,ou=People,dc=example,dc=com alice"
-docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=alice,ou=People,dc=example,dc=com bob"
+docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=bob,ou=People,dc=example,dc=com bob"
 
 #docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=httpalfresco,ou=People,dc=example,dc=com httpalfresco"
 # Add principles for KeyCloak and generate keytab
