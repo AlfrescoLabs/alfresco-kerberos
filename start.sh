5372764642ef0491d10e319e1dfcbf98f1ac3be7
dhrn <dharan.g@muraai.com>
2021-07-18 16:05:17 +0530

    * fix

--- start.sh
+++ start.sh
@@ -17,9 +17,13 @@ docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=alice
 #docker exec -ti kerberos kadmin.local -q "addprinc -pw password  -x dn=uid=httpalfresco,ou=People,dc=example,dc=com httpalfresco"
 # Add principles for KeyCloak and generate keytab
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpalfresco,ou=People,dc=example,dc=com HTTP/example.com@EXAMPLE.COM"
-docker exec -ti kerberos kadmin.local -q "ktadd -k /etc/keytabs/alfresco.keytab HTTP/example.com@EXAMPLE.COM"
+docker exec -ti kerberos kadmin.local -q "ktadd -k alfresco.keytab HTTP/example.com@EXAMPLE.COM"
 
 docker exec -ti kerberos kadmin.local -q "list_principals"
+
+KERBEROS = docker-compose ps -q kerberos;
+docker cp $(KERBEROS):/alfresco.keytab ./keytabs/
+chmod 777 keytabs
 
 docker-compose restart alfresco
 
