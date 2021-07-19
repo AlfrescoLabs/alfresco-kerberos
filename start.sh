d10faa3d22b3cb602b39f16461aa32ce073c2756
dhrn <dharan.g@muraai.com>
2021-07-19 13:49:58 +0530

    * fix

--- start.sh
+++ start.sh
@@ -24,8 +24,8 @@ docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpal
 docker exec -ti kerberos kadmin.local -q "ktadd -k alfresco.keytab HTTP/example.com@EXAMPLE.COM"
 
 # Add principles for Share and generate keytab
-docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpshare,ou=People,dc=example,dc=com HTTP/example.com@EXAMPLE.COM"
-docker exec -ti kerberos kadmin.local -q "ktadd -k share.keytab HTTP/example.com@EXAMPLE.COM"
+#docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpshare,ou=People,dc=example,dc=com HTTP/example.com@EXAMPLE.COM"
+#docker exec -ti kerberos kadmin.local -q "ktadd -k share.keytab HTTP/example.com@EXAMPLE.COM"
 
 printf "\n======== Available principles in the kerberos ========\n\n"
 docker exec -ti kerberos kadmin.local -q "list_principals"
@@ -39,9 +39,9 @@ docker cp ${KERBEROS}:/alfresco.keytab ./keytabs/
 chmod 777 ./keytabs/alfresco.keytab
 docker cp ./keytabs/alfresco.keytab ${ALFRESCO}:/etc/alfresco.keytab
 
-docker cp ${KERBEROS}:/share.keytab ./keytabs/
-chmod 777 ./keytabs/share.keytab
-docker cp ./keytabs/share.keytab ${SHARE}:/etc/share.keytab
+#docker cp ${KERBEROS}:/share.keytab ./keytabs/
+#chmod 777 ./keytabs/share.keytab
+#docker cp ./keytabs/share.keytab ${SHARE}:/etc/share.keytab
 
 docker-compose restart alfresco
 
