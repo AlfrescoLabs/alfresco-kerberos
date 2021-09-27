2ee269c6029a591ef1756d4f6b16439245d3795c
dhrn <dharan.g@muraai.com>
2021-09-27 18:25:59 +0530

    * mior corrections

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ printf "\n======== stop the running env ========\n\n"
 
 docker-compose down -v;
 sleep 10;
-rm -rf ./keytabs/*keytab;
+rm -rf .keytabs/*keytab && mkdir .keytabs;
 
 printf "\n======== build and run docker images ========\n\n"
 docker-compose build;
@@ -25,7 +25,7 @@ docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpal
 docker exec -ti kerberos kadmin.local -q "ktadd -k alfresco.keytab HTTP/example.com@EXAMPLE.COM"
 # Add principles for Process and generate keytab
 docker exec -ti kerberos kadmin.local -q "addprinc -pw password -x dn=uid=httpprocess,ou=People,dc=example,dc=com HTTP/example.com@EXAMPLE.COM"
-docker exec -ti kerberos kadmin.local -q "ktadd -k process HTTP/example.com@EXAMPLE.COM"
+docker exec -ti kerberos kadmin.local -q "ktadd -k process.keytab HTTP/example.com@EXAMPLE.COM"
 
 printf "\n======== Available principles in the kerberos ========\n\n"
 docker exec -ti kerberos kadmin.local -q "list_principals"
@@ -36,14 +36,14 @@ ALFRESCO=$(docker-compose ps -q alfresco);
 SHARE=$(docker-compose ps -q share);
 PROCESS=$(docker-compose ps -q process);
 
-docker cp ${KERBEROS}:/alfresco.keytab ./keytabs/
-docker cp ${PROCESS}:/process.keytab ./keytabs/
-chmod 777 ./keytabs/alfresco.keytab
-chmod 777 ./keytabs/process.keytab
+docker cp ${KERBEROS}:/alfresco.keytab .keytabs/
+docker cp ${PROCESS}:/process.keytab .keytabs/
+chmod 777 .keytabs/alfresco.keytab
+chmod 777 .keytabs/process.keytab
 
-docker cp ./keytabs/alfresco.keytab ${ALFRESCO}:/etc/alfresco.keytab
-docker cp ./keytabs/alfresco.keytab ${SHARE}:/etc/share.keytab
-docker cp ./keytabs/process.keytab ${PROCES}:/etc/process.keytab
+docker cp .keytabs/alfresco.keytab ${ALFRESCO}:/etc/alfresco.keytab
+docker cp .keytabs/alfresco.keytab ${SHARE}:/etc/share.keytab
+docker cp .keytabs/process.keytab ${PROCES}:/etc/process.keytab
 
 docker-compose restart alfresco
 docker-compose restart share
